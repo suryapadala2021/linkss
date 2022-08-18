c5197a91c4418786ba2d27f9b9d31978fdad02b9
suryapadala2021 <suryapadala2021@gmail.com>
2022-08-18 08:18:01 +0000

    ok

--- src/components/TeamMatches/index.js
+++ src/components/TeamMatches/index.js
@@ -1,12 +1,71 @@
-import {component} from 'react'
+import {Component} from 'react'
 import './index.css'
 import Loader from 'react-loader-spinner'
 
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 
-class TeamMatches extends component {
+class TeamMatches extends Component {
+  state = {isLoading: true}
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  getData = async () => {
+    const {match} = this.props
+    const {id} = match.params
+    const response = await fetch(`https://apis.ccbp.in/ipl/${id}`)
+    const data = await response.json()
+    const update = {
+      recentMatches: data.recent_matches.map(each => ({
+        competingTeam: each.competing_team,
+        competingTeamLogo: each.competing_team_logo,
+        date: each.date,
+        firstInnings: each.first_innings,
+        id: each.id,
+        manOfTheMatch: each.man_of_the_match,
+        matchStatus: each.match_status,
+        result: each.result,
+        secondInnings: each.second_innings,
+        umpires: each.umpires,
+        venue: each.venue,
+      })),
+      latestMatchDetails: {
+        competingTeam: data.latest_match_details.competing_team,
+        competingTeamLogo: data.latest_match_details.competing_team_logo,
+        date: data.latest_match_details.date,
+        firstInnings: data.latest_match_details.first_innings,
+        id: data.latest_match_details.id,
+        manOfTheMatch: data.latest_match_details.man_of_the_match,
+        matchStatus: data.latest_match_details.match_status,
+        result: data.latest_match_details.result,
+        secondInnings: data.latest_match_details.second_innings,
+        umpires: data.latest_match_details.umpires,
+        venue: data.latest_match_details.venue,
+      },
+      teamBannerUrl: data.team_banner_url,
+    }
+    console.log(update.recentMatches)
+    console.log(data)
+  }
+
   render() {
-    return <h1>surya</h1>
+    const {isLoading} = this.state
+    return (
+      <div className="team-matches-container">
+        {isLoading && (
+          <div testid="loader">
+            <Loader
+              className="loader"
+              type="Oval"
+              color="#ffffff"
+              height={50}
+              width={50}
+            />{' '}
+          </div>
+        )}
+      </div>
+    )
   }
 }
 
